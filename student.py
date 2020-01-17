adb0458b9e61fe50b4d116166893124ad783fe73
Manila Bui <manila.bui@gmail.com>
2020-01-16 22:36:53 -0600

    exercise with challenge

---
+++ student.py
@@ -0,0 +1,18 @@
+class Student:
+		def __init__(self, first_name, last_name):
+				self.first_name = first_name
+				self.last_name = last_name
+				self.exercises = list()
+
+		def __str__(self):
+				exercise_names = ('\n * ').join([ exercise.name for exercise in self.exercises ])
+				return f'{self.first_name} {self.last_name} is working on: \n * {exercise_names}'
+
+		def add_slack(self, handle):
+				self.slack = handle
+
+		def set_cohort(self, cohort):
+				self.cohort = cohort
+
+		def add_exercises(self, exercises):
+				self.exercises.extend(exercises)
